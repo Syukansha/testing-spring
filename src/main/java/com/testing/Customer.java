fb63fc9f50848fe3f37185d3d37282bafc9e2b1f
syukriishak <syukriishak622@gmail.com>
2022-12-09 14:53:40 +0800

    update

--- src/main/java/com/testing/Customer.java
+++ src/main/java/com/testing/Customer.java
@@ -1,25 +1,54 @@
 package com.testing;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
     private Integer age;
 
+    public Customer(){
 
-    public Customer(Integer id, String name, String email, Integer age){
+    }
+
+    public Customer(Integer id, String name, String email, Integer age) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.age = age;
     }
 
-    public Integer getId(){ return id;}
-    public String getName(){return name;}
-    public String getEmail(){return email;}
-    public Integer getAge(){return age;}
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
 
     public void setId(Integer id) {
         this.id = id;
